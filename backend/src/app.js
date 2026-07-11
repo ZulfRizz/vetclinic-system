c8a27829e73bb8547b38336faa91d63f6b62f2e2
ZulfRizz <zulfrianwar@gmail.com>
2026-07-12 05:16:38 +0700

    feat: setup Express application

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const { sequelize } = require('./models');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('api/test',(req,res) =>{
+    res.json({message: 'BE + PostgreSQL OK!!'});
+});
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync({alter: true})
+    .then(() => {
+        console.log('DB OK!!');
+        app.listen(PORT,() =>{
+            console.log(`server jalan di localhost port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('koneksi DB gagal: ',err);
+    })
---
+++ backend/src/models/index.js
@@ -0,0 +1,7 @@
+const sequelize = require('.sequelize,./config/database');
+const User = require('./User');
+
+module.exports = {
+    sequelize,
+    User,
+};
